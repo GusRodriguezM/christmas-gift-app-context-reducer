f6f84735652b0912966bac4087f46d2f51d08edf
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-05-03 00:05:26 -0500

    Step 3: Adding a form to add a gift to the list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
+import { useState } from 'react';
+import { useForm } from './hooks/useForm';
 import './App.css';
 
-const gifts = [
-  {id: 1, name: 'socks'},
-  {id: 2, name: 'ugly sweater'},
-  {id: 3, name: 'Santa\'s hat'},
-  {id: 4, name: 'snow sled'},
-  {id: 5, name: 'snowball gun'},
-];
-
 export const App = () => {
+
+  const [gifts, setGifts] = useState([
+    {id: 1, name: 'socks'},
+    {id: 2, name: 'ugly sweater'},
+    {id: 3, name: 'Santa\'s hat'},
+    {id: 4, name: 'snow sled'},
+    {id: 5, name: 'snowball gun'},
+  ]);
+
+  const [formValues, handleInputChange, reset] = useForm({
+    id: (+new Date()).toString(),
+    name: ''
+  });
+
+  const { name } = formValues;
+
+  const handleAddGift = (e) => {
+    e.preventDefault();
+    setGifts([...gifts, formValues]);
+    reset();
+  }
+
+  console.log(gifts);
+
   return (
     <div className='App'>
 
       <div className='list'>
         <h1>Gifts</h1>
 
-        {
-          gifts.map(gift => (
-            <ul key={gift.id}>
-              {gift.name}
-            </ul>
-          ))
-        }
+        <form onSubmit={handleAddGift}>
+          <input
+            type='text'
+            placeholder='Your gift'
+            name='name'
+            value={name}
+            autoComplete='off'
+            onChange={handleInputChange}
+          />
+
+          <button
+            type='submit'
+          >
+            Add a gift
+          </button>
+        </form>
+
+        <ul>
+          {
+            gifts.map(gift => (
+              <li key={gift.id}>
+                {gift.name}
+              </li>
+            ))
+          }
+        </ul>
+
       </div>
-      
+
     </div>
   );
 }
