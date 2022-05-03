7b63c5f009373b2a5727bf18b5598c7d368daf83
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-05-02 21:47:11 -0500

    Step 1: Showing a static list of gifts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const gifts = [
+  {id: 1, name: 'socks'},
+  {id: 2, name: 'ugly sweater'},
+  {id: 3, name: 'Santa\'s hat'},
+  {id: 4, name: 'snow sled'},
+  {id: 5, name: 'snowball gun'},
+];
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+      <h1>Gifts</h1>
+
+      {
+        gifts.map(gift => (
+          <ul key={gift.id}>
+            {gift.name}
+          </ul>
+        ))
+      }
     </div>
   );
 }
-
-export default App;
