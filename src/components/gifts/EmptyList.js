985757bc03e0ed6b2f2d9c898576a93f1a6ad7dd
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-07-26 00:04:52 -0500

    Extra: Adding styles to the empty list component

--- src/components/gifts/EmptyList.js
+++ src/components/gifts/EmptyList.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const EmptyList = () => {
     return (
-        <div>
-            <span>Your list is empty</span>
+        <div className='empty'>
+            <h1>Your list is empty</h1>
+            <i className="fa-solid fa-face-sad-tear"></i>
         </div>
     )
 }
