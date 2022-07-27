47098da04a00175ea41f4acc9d58258b0a46f0ce
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-07-27 01:28:18 -0500

    Extra: Adding an error message when a field is wrong in the login form

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { login } from '../../actions/auth';
 import { AuthContext } from '../../context/AuthContext';
 import { useForm } from '../../hooks/useForm';
+import validator from 'validator';
 
 export const LoginScreen = () => {
 
     const { dispatchLogin } = useContext(AuthContext);
+    const [message, setMessage] = useState('');
+    const [visible, setVisible] = useState(false);
 
     const [formValues, handleInputChange] = useForm({
         name: '',
@@ -15,13 +18,49 @@ export const LoginScreen = () => {
 
     const { name, email, password } = formValues;
 
+    const isFormValid = () => {
+        if(name.trim().length === 0 && email.trim().length === 0 && password.trim().length === 0){
+            setMessage('These fields are required');
+            setVisible(true);
+            return false;
+        }else if(!validator.isEmail(email)){
+            setMessage('The email is not valid');
+            setVisible(true);
+            return false;
+        }else if(name.trim().length === 0){
+            setMessage('The name is required');
+            setVisible(true);
+            return false;
+        }else if(email.trim().length === 0){
+            setMessage('The email is required');
+            setVisible(true);
+            return false;
+        }else if(password.trim().length === 0){
+            setMessage('The password is required');
+            setVisible(true);
+            return false;
+        }
+
+        setMessage('');
+        setVisible(false);
+        return true;
+    }
+
     const handleLogin = (e) => {
         e.preventDefault();
-        dispatchLogin( login({ name, email, password }) );
+
+        if(isFormValid()){
+            dispatchLogin( login({ name, email, password }) );
+        }
     }
 
     return (
         <div className='login'>
+
+            <div className={`login__message ${!visible && 'display'}`}>
+                <i className="fa-solid fa-circle-exclamation"></i>
+                <h4>{message}</h4>
+            </div>
 
             <h1 className='login__title'>Login</h1>
 
@@ -48,7 +87,7 @@ export const LoginScreen = () => {
                 />
 
                 <input 
-                    type='text'
+                    type='password'
                     placeholder='Password'
                     name='password'
                     className='login__form--input'
